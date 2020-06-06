f0788b3493a4a9549fde38626e8628e66ade6ccf
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-06-06 11:10:40 +0000

    [add]tag-search

--- app/controllers/comics_controller.rb
+++ app/controllers/comics_controller.rb
@@ -1,6 +1,9 @@
 class ComicsController < ApplicationController
 	def index
 		@comics = Comic.all
+		if params[:tag_name]
+		@comics = Comic.tagged_with("#{params[:tag_name]}")
+	    end
 	end
 
 	def show
@@ -8,5 +11,10 @@ class ComicsController < ApplicationController
 		@comment = Comment.new
 		@user = User.find(params[:id])
 	end
+    private
+
+    def comic_params
+    params.require(:comic).permit(:tag_list)
+    end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,8 +44,7 @@ Rails.application.routes.draw do
   get 'withdraw' => 'users#withdraw'
 
   #検索
-  get '/search', to: 'searches#search'
-
+  get '/search' => 'searches#search'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
