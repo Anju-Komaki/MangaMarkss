242922ecccacadd01a5c2f67269d6a540a6ec932
Anju-Komaki <upfmdb1124ad@gmail.com>
2020-06-15 12:51:13 +0000

    [add]rspec controller test

---
+++ spec/requests/admins_category_request_spec.rb
@@ -0,0 +1,89 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::Categories controller", type: :request do
+	describe '管理者カテゴリー機能' do
+		let(:admin) { create(:admin) }
+		let(:category) { create(:category) }
+		before do
+			visit new_admin_session_path
+			fill_in 'admin[username]', with: admin.username
+			fill_in 'admin[password]', with: admin.password
+			click_button 'ログイン'
+
+			visit admins_categories_path
+			fill_in 'category[name]',with: 'category'
+			click_button 'submit'
+		end
+
+		describe 'カテゴリー一覧' do
+			context '表示の確認' do
+				it 'カテゴリー一覧と表示されている' do
+					visit admins_categories_path
+					expect(page).to have_content 'カテゴリー一覧'
+				end
+				it 'カテゴリー名フォームが表示されている' do
+					visit admins_categories_path
+					expect(page).to have_field 'category[name]'
+				end
+				it 'カテゴリー名が表示されている' do
+					visit admins_categories_path
+					expect(page).to have_content 'category'
+				end
+				it 'カテゴリー名の編集リンクが表示されている' do
+					visit admins_categories_path
+					expect(page).to have_link 'edit'
+				end
+				it 'カテゴリー名の削除リンクが表示されている' do
+					visit admins_categories_path
+					expect(page).to have_link 'destroy'
+				end
+			end
+			context 'カテゴリーの追加' do
+				it 'カテゴリーの追加に成功する' do
+					visit admins_categories_path
+					fill_in 'category[name]',with: 'category.name'
+					click_button 'submit'
+
+					expect(current_path).to eq('/admins/categories')
+				end
+
+				it 'カテゴリーの追加に失敗する' do
+					visit admins_categories_path
+					fill_in 'category[name]',with: ''
+					click_button 'submit'
+
+					expect(current_path).to eq('/admins/categories')
+				end
+			end
+		end
+
+		describe 'カテゴリーの編集' do
+			context '表示の確認' do
+				it 'カテゴリー編集と表示されている' do
+					visit edit_admins_category_path(category)
+					expect(page).to have_content 'カテゴリー編集'
+				end
+				it 'カテゴリー追加フォームが表示されている' do
+					visit edit_admins_category_path(category)
+					expect(page).to have_field 'category[name]', with: category.name
+				end
+			end
+			context 'カテゴリー名の編集' do
+				it 'カテゴリー名の編集に成功する' do
+					visit edit_admins_category_path(category)
+					fill_in 'category[name]',with: category.name
+					click_button 'submit'
+
+					expect(page).to have_content "カテゴリー名を変更しました"
+				end
+				it 'カテゴリー名の編集に失敗する' do
+					visit edit_admins_category_path(category)
+					fill_in 'category[name]',with: ''
+					click_button 'submit'
+
+					expect(page).to have_content "カテゴリー名を変更できませんでした"
+				end
+			end
+		end
+	end
+end
--- spec/requests/informations_request_spec.rb
+++ spec/requests/informations_request_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 
+RSpec.describe "Admin::Information controller", type: :request do
 describe 'お知らせのテスト' do
+	let(:admin) { create(:admin) }
 	let(:information) { create(:information) }
 	describe 'お知らせ一覧' do
 		before do
+			visit new_admin_session_path
+			fill_in 'admin[username]', with: admin.username
+			fill_in 'admin[password]', with: admin.password
+			click_button 'ログイン'
+
+			visit new_admins_information_path
+			fill_in 'information[title]', with: information.title
+			fill_in 'information[body]', with: information.body
+			click_button '保存'
+
 			informations_path
 		end
 		context '表示の確認' do
@@ -11,6 +23,15 @@ describe 'お知らせのテスト' do
 				visit informations_path
 				expect(page).to have_content 'お知らせ'
 			end
+			it 'お知らせのタイトルが表示される' do
+				visit informations_path
+				expect(page).to have_content information.title
+			end
+			it 'お知らせの内容の一部が表示される' do
+				visit informations_path
+				expect(page).to have_content information.body
+			end
 		end
 	end
+end
 end
